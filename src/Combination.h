e38428e640cd81cee66b7bdb9203b454cd566576
wceballos <wilminc@gmail.com>
2019-10-06 07:09:00 -0400

    Tested new template class

--- src/Combination.h
+++ src/Combination.h
@@ -12,13 +12,11 @@
 
 template <class T>
 class Combination {
+
 private:
   int _startAddr, _length;
-  void _read();
 
 public:
-  //T *data; /*!< The current combination */
-
   //! Constructor.
   /*!
    * The constructor will automatically load the data from the EEPROM.
@@ -26,7 +24,7 @@ public:
    * \param startAddr Starting address of combination in EEPROM.
    * \param length Length in bytes of the combination.
    */
-  Combination(const int &startAddr, const int &length) {
+  Combination(size_t length, int startAddr = 0) {
     _startAddr = startAddr;
     _length = length;
   }
@@ -35,63 +33,76 @@ public:
   ~Combination() {
   }
 
-  //! Update the combination.
+  //! Save a combination to memory.
   /*!
-   * Copies array passed as a parameter to member data array and then
-   * writes it to EEPROM.
+   * Saves the array in the argument on the EEPROM. This will overwrite
+   * any previous saves.
    *
-   * \param newCombination The combination that will overwrite the current.
+   * \param combinationArray The combination that will overwrite the current.
    */
-  void write(T *newCombination);
+  void save(T *combinationArray);
+
+  //! Loads the combination from memory.
+  /*!
+   * The combination stored in the EEPROM is loaded into the array passed as an
+   * argument. Warning: the array must be able to fit the combination.
+   *
+   * \param combinationArray The array to load the combination to.
+   */
+  void load(T *combinationArray);
 
   //! Check if the combination matches a given array.
   /*!
-   * \param operand The array to compare to.
+   *
+   * The combination saved in the EEPROM will be compared to the combination
+   * in the array passed as an argument.
+   *
+   * \param combinationArray The array to compare to.
    * \return true if the combination matches the array given.
    */
-  bool equals(T *operand);
+  bool equals(T *combinationArray);
 
- //! Get the value of an item at an index.
- /*! 
- * \param index Index of item.
- * \return item.
- */
+  //! Get the value of an element at an index.
+  /*! 
+  * \param index Index of item.
+  * \return item.
+  */
   T get(int index);
 };
 
 template <class T>
-void Combination<T>::write(T *newCombination) {
+void Combination<T>::save(T *combinationArray) {
   int eeAddr = _startAddr;
   int incr = sizeof(T);
-  for(int i = 0; i < _length; ++i) {
-    EEPROM.put(eeAddr, newCombination[i]);
+  for(int i = 0; i < _length; i++) {
+    EEPROM.put(eeAddr, combinationArray[i]);
     eeAddr += incr;
   }
 }
 
-/*!
- * \param operand An array to compare to data.
- * \return true if all array elements equal of operand equal the elements of
- *  member variable "data".
- */
 template <class T>
-bool Combination<T>::equals(T *operand) {
+void Combination<T>::load(T *combinationArray) {
+  int eeAddr = _startAddr;
+  int incr = sizeof(T);
+  for(int i = 0; i < _length; i++) {
+    EEPROM.get(eeAddr, combinationArray[i]);
+    eeAddr += incr;
+  }
+}
+
+template <class T>
+bool Combination<T>::equals(T *combinationArray) {
   int eeAddr = _startAddr;
   int incr = sizeof(T);
   T data;
-  for(int i = 0; i < _length; ++i) {
+  for(int i = 0; i < _length; i++) {
     EEPROM.get(eeAddr, data);
     eeAddr += incr;
-    if(operand[i] != data) return false;
+    if(combinationArray[i] != data) return false;
   }
   return true;
 }
 
- //! Get the value of an item at an index.
- /*! 
- * \param index Index of item.
- * \return item.
- */
 template <class T>
 T Combination<T>::get(int index) {
   int eeAddr =  _startAddr + sizeof(T) * index;
