177bbcefd390b7bfabb7d59afecfd46f3fcab997
wceballos <wilminc@gmail.com>
2019-10-06 04:55:13 -0400

    Testing using a template class

--- src/Combination.h
+++ src/Combination.h
@@ -1,16 +1,23 @@
+//! Combination.h
+/*!
+ * The Combination class reads and writes a combination to and from EEPROM.
+ * A combination must be an array type.
+ *
+ * \version Verison 3.0.
+ */
 #ifndef COMBINATION_H
 #define COMBINATION_H
 
 #include <EEPROM.h>
 
+template <class T>
 class Combination {
 private:
   int _startAddr, _length;
-  uint8_t *_data; /*!< The current combination */
-
   void _read();
 
 public:
+  //T *data; /*!< The current combination */
 
   //! Constructor.
   /*!
@@ -22,29 +29,75 @@ public:
   Combination(const int &startAddr, const int &length) {
     _startAddr = startAddr;
     _length = length;
-    _data = new uint8_t[_length];
-    _read();
   }
 
   //! Destructor.
   ~Combination() {
-    delete[] _data;
   }
 
   //! Update the combination.
   /*!
-   * The combination will be changed both in memory and EEPROM.
+   * Copies array passed as a parameter to member data array and then
+   * writes it to EEPROM.
    *
    * \param newCombination The combination that will overwrite the current.
    */
-  void write(char *newCombination);
+  void write(T *newCombination);
 
   //! Check if the combination matches a given array.
   /*!
    * \param operand The array to compare to.
    * \return true if the combination matches the array given.
    */
-  bool equals(char *operand);
+  bool equals(T *operand);
+
+ //! Get the value of an item at an index.
+ /*! 
+ * \param index Index of item.
+ * \return item.
+ */
+  T get(int index);
 };
+
+template <class T>
+void Combination<T>::write(T *newCombination) {
+  int eeAddr = _startAddr;
+  int incr = sizeof(T);
+  for(int i = 0; i < _length; ++i) {
+    EEPROM.put(eeAddr, newCombination[i]);
+    eeAddr += incr;
+  }
+}
+
+/*!
+ * \param operand An array to compare to data.
+ * \return true if all array elements equal of operand equal the elements of
+ *  member variable "data".
+ */
+template <class T>
+bool Combination<T>::equals(T *operand) {
+  int eeAddr = _startAddr;
+  int incr = sizeof(T);
+  T data;
+  for(int i = 0; i < _length; ++i) {
+    EEPROM.get(eeAddr, data);
+    eeAddr += incr;
+    if(operand[i] != data) return false;
+  }
+  return true;
+}
+
+ //! Get the value of an item at an index.
+ /*! 
+ * \param index Index of item.
+ * \return item.
+ */
+template <class T>
+T Combination<T>::get(int index) {
+  int eeAddr =  _startAddr + sizeof(T) * index;
+  T data;
+  EEPROM.get(eeAddr, data);
+  return data;
+}
 
 #endif
